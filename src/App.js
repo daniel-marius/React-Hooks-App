9a8445108ab67e05917204820b24e2d0f1163118
danielmarius1995 <50268474+danielmarius1995@users.noreply.github.com>
2020-07-15 18:06:38 +0300

    First commit

---
+++ src/App.js
@@ -0,0 +1,69 @@
+import React, { useState } from 'react';
+
+import Accordion from './components/Accordion';
+import Search from './components/Search';
+import Dropdown from './components/Dropdown';
+import Translate from './components/Translate';
+import Route from './components/Route';
+import Header from './components/Header';
+
+const items = [
+  {
+    title: 'Title1',
+    content: 'Content1'
+  },
+  {
+    title: 'Title2',
+    content: 'Content2'
+  },
+  {
+    title: 'Title3',
+    content: 'Content3'
+  },
+  {
+    title: 'Title4',
+    content: 'Content4'
+  }
+];
+
+const options = [
+  {
+    label: 'The Color Red',
+    value: 'red'
+  },
+  {
+    label: 'The Color Green',
+    value: 'green'
+  },
+  {
+    label: 'The Color Blue',
+    value: 'blue'
+  }
+];
+
+export default () => {
+  const [selected, setSelected] = useState(options[0]);
+
+  return (
+    <div className="ui container">
+      <Header />
+      <Route path="/">
+        <Accordion items={items} />
+      </Route>
+      <Route path="/list">
+        <Search />
+      </Route>
+      <Route path="/dropdown">
+        <Dropdown
+          label="Select a Color"
+          options={options}
+          selected={selected}
+          onSelectedChange={setSelected}
+        />
+      </Route>
+      <Route path="/translate">
+        <Translate />
+      </Route>
+    </div>
+  );
+};
